55518faeeb1e7a1e133c6bbd09db9257d93b96fc
Rachel Haas <rachelhaas@ufl.edu>
2019-04-21 18:42:12 -0400

    added about page html/css

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,3 +1,19 @@
-$("button").on("click", function() {
-  $("body").toggleClass("open");
+$( document ).ready(function() {
+
+$( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
+
 });
